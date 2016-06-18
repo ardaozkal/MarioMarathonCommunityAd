4b70383157bbfd74de3c19a35b9a3f2eeef6e2f9
Arda Özkal <arda.ozkal@studentpartner.com>
2016-06-18 20:07:58 +0300

    Centered Text

--- mario.php
+++ mario.php
@@ -1,14 +1,21 @@
 <?php
 header('Content-type: image/png');
 $donationImage = imagecreatefrompng("img.png");
+$json = json_decode(utf8_encode(file_get_contents("http://www.mariomarathon.com/rest/partners/arqade.com")));
+
 $white = imagecolorallocate($donationImage, 255, 255, 255);
 
-$json = json_decode(utf8_encode(file_get_contents("http://www.mariomarathon.com/rest/partners/arqade.com")));
 $total = round($json->total);
 $byus = $json->domainTotal;
 
-imagettftext($donationImage,30,0,60,450,$white,'arial.ttf',"$".$total);
-imagettftext($donationImage,30,0,370,450,$white,'arial.ttf',"$".$byus);
+$bbox = imagettfbbox(30, 0, 'arial.ttf', "$".$total);
+$bbox = imagettfbbox(30, 0, 'arial.ttf', "$".$byus);
+
+$x = $bbox[0] + (226 / 2) - ($bbox[4] / 2);
+$x2 = $bbox2[0] + (578 / 2) - ($bbox2[4] / 2) + 75;
+
+imagettftext($donationImage,30,0,$x,450,$white,'arial.ttf',"$".$total);
+imagettftext($donationImage,30,0,$x2,450,$white,'arial.ttf',"$".$byus);
 imagepng($donationImage);
 imagedestroy($donationImage);
 ?>
