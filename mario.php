1fa203c26f98b2596cef58a29a1a76a88306a709
Arda Özkal <arda.ozkal@studentpartner.com>
2016-06-18 20:06:47 +0300

    Beta and Live Test

--- mario.php
+++ mario.php
@@ -3,18 +3,9 @@ header('Content-type: image/png');
 $donationImage = imagecreatefrompng("img.png");
 $white = imagecolorallocate($donationImage, 255, 255, 255);
 
-$json = file_get_contents("http://www.mariomarathon.com/rest/partners/arqade.com");
-
-$jsonIterator = new RecursiveIteratorIterator(
-    new RecursiveArrayIterator(json_decode($json, TRUE)),
-    RecursiveIteratorIterator::SELF_FIRST);
-
-foreach ($jsonIterator as $key => $val) {
-    if(!is_array($val)) {
-        if ($key == "total") { $total = round($val); }
-        else if ($key == "domainTotal") { $byus = $val; }
-    }
-}
+$json = json_decode(utf8_encode(file_get_contents("http://www.mariomarathon.com/rest/partners/arqade.com")));
+$total = round($json->total);
+$byus = $json->domainTotal;
 
 imagettftext($donationImage,30,0,60,450,$white,'arial.ttf',"$".$total);
 imagettftext($donationImage,30,0,370,450,$white,'arial.ttf',"$".$byus);
