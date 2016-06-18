28e48cddf977472f5355fd7ff4aaebc232840842
Arda Özkal <arda.ozkal@studentpartner.com>
2016-06-18 20:23:29 +0300

    Switch to dosis

--- mario.php
+++ mario.php
@@ -8,14 +8,14 @@ $white = imagecolorallocate($donationImage, 255, 255, 255);
 $total = round($json->total);
 $byus = $json->domainTotal;
 
-$bbox = imagettfbbox(30, 0, 'arial.ttf', "$".$total);
-$bbox = imagettfbbox(30, 0, 'arial.ttf', "$".$byus);
+$bbox = imagettfbbox(40, 0, 'dosis.ttf', "$".$total);
+$bbox = imagettfbbox(40, 0, 'dosis.ttf', "$".$byus);
 
 $x = $bbox[0] + (226 / 2) - ($bbox[4] / 2);
 $x2 = $bbox2[0] + (578 / 2) - ($bbox2[4] / 2) + 75;
 
-imagettftext($donationImage,30,0,$x,450,$white,'arial.ttf',"$".$total);
-imagettftext($donationImage,30,0,$x2,450,$white,'arial.ttf',"$".$byus);
+imagettftext($donationImage,40,0,$x,460,$white,'dosis.ttf',"$".$total);
+imagettftext($donationImage,40,0,$x2,460,$white,'dosis.ttf',"$".$byus);
 imagepng($donationImage);
 imagedestroy($donationImage);
 ?>
